35f0896c9f6b7b21f70cc4e60f79e9e823e1820b
Joachim V. Hasseldam <johas@dfds.com>
2020-07-15 23:28:17 +0200

    - Solved kata "GreedIsGood" - Organize fsproj file

---
+++ GreedIsGood.fs
@@ -0,0 +1,25 @@
+module GreedIsGood
+// https://www.codewars.com/kata/5270d0d18625160ada0000e4/train/fsharp
+// 5 kyu
+
+let scoreThreeOfTheSame = function
+  | 1 -> 1000
+  | 6 -> 600
+  | 5 -> 500
+  | 4 -> 400
+  | 3 -> 300
+  | 2 -> 200
+  | _ -> 0
+
+let scoreSideCount sideCount =
+  let rec loop score sideCount =
+    match sideCount with
+    | x, count when count >= 3 -> loop (scoreThreeOfTheSame x) (x, count - 3)
+    | 1, count -> score + count * 100
+    | 5, count -> score + count * 50
+    | _ -> score + 0
+  loop 0 sideCount
+
+let score (dice : int list) =
+  List.countBy id dice
+  |> List.fold (fun score sideCount -> score + (scoreSideCount sideCount)) 0 
